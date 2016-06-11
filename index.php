4ca885880302ed88e793d09d2722feb96a6982f1
Ralf Patrik Blaga <ralf.patric@yahoo.com>
2016-06-11 18:50:48 +0200

    carousel in progress2

--- index.php
+++ index.php
@@ -11,45 +11,51 @@ $carouselID = $carousel->post->ID;
 
 <div class="carousel-container full-height col-md-12">
 
-    <div class="row">
-        <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="10000">
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
-                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                    <div class="item active">
-                        <div class="upper"
-                             style="background: url(<?php the_field('image1', 5); ?>);
-                                 background-size: cover;">
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="upper"
-                             style="background: url(<?php the_field('image2', 5); ?>);
-                                 background-size: cover;">
-                        </div>
-                    </div>
-                    <div class="item active">
-                        <div class="upper"
-                             style="background: url(<?php the_field('image3', 5); ?>);
-                                 background-size: cover;">
-                            <div class="text">
+    <div class="container">
+
+        <div class="row center-block">
+            <div id="myCarousel" class="carousel slide center-block col-md-12" data-ride="carousel" data-interval="10000">
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner center-block" role="listbox">
+                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                        <div class="item active">
+                            <div class="upper center-block"
+                                 style="background: url(<?php the_field('image1', 5); ?>);
+                                     background-size: cover; background-position: center;">
                             </div>
                         </div>
-                    </div>
-                <?php endwhile; else: ?>
-                    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-            </div>
+                        <div class="item">
+                            <div class="upper cente-block"
+                                 style="background: url(<?php the_field('image2', 5); ?>);
+                                     background-size: cover; background-position: center;">
+                            </div>
+                        </div>
+                        <div class="item center-block">
+                            <div class="upper"
+                                 style="background: url(<?php the_field('image3', 5); ?>);
+                                     background-size: cover; background-position: center;">
+                            </div>
+                        </div>
+                    <?php endwhile; else: ?>
+                        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
+                </div>
 
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
         </div>
+
+        <a href="#">
+            <span class="glyphicon glyphicon-chevron-down"></span>
+        </a>
+
     </div>
 
 </div>
