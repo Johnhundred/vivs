631d38576831f49aab80ba558008d5c30f7dfdd0
Johannes Skjærbæk <john@particledesigns.dk>
2016-06-11 14:02:03 +0200

    enqueued style.css

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+function Vivs_scripts() {
+    wp_enqueue_style( 'style-vivs', get_stylesheet_uri() );
+    wp_enqueue_script( 'script-vivs', get_template_directory_uri() . '/js/vivs.js', array(), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'Vivs_scripts' );
+
+?>
