07c9adb49cc887fb089ab4b71cc0dca39ba9c45d
Johannes Skjærbæk <john@particledesigns.dk>
2016-06-11 14:45:31 +0200

    index hooked up to social, contact, about pages backend

--- index.php
+++ index.php
@@ -6,7 +6,17 @@ get_header(); ?>
 
 </div>
 
+<?php
+$about = get_post(7);
+$aboutTitle = $about->post_title;
+$aboutContent = $about->post_content;
+?>
+
 <div class="about-container full-height col-md-12">
+
+    <h2><?php echo $aboutTitle; ?></h2>
+
+    <?php echo $aboutContent; ?>
 
 </div>
 
@@ -14,11 +24,31 @@ get_header(); ?>
 
 </div>
 
+<?php
+$social = get_post(9);
+$socialTitle = $social->post_title;
+$socialContent = $social->post_content;
+?>
+
 <div class="social-container full-height col-md-12">
+
+    <h2><?php echo $socialTitle; ?></h2>
+
+    <?php echo $socialContent; ?>
 
 </div>
 
+<?php
+$contact = get_post(11);
+$contactTitle = $contact->post_title;
+$contactContent = $contact->post_content;
+?>
+
 <div class="contact-container full-height col-md-12">
+
+    <h2><?php echo $contactTitle; ?></h2>
+
+    <?php echo $contactContent; ?>
 
 </div>
 
