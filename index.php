6f19d95535e452264dfbb09d271cfd4a829727db
Johannes Skjærbæk <john@particledesigns.dk>
2016-06-13 21:34:54 +0200

    about modal done

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $carouselID = $carousel->post->ID;
     <div class="container">
 
         <div class="row center-block">
-            <div id="myCarousel" class="carousel slide center-block col-md-12" data-ride="carousel" data-interval="10000">
+            <div id="myCarousel" class="carousel slide center-block col-md-12" data-ride="carousel" data-interval="5000">
                 <!-- Wrapper for slides -->
                 <div class="carousel-inner center-block" role="listbox">
                     <div class="item active">
@@ -71,19 +71,35 @@ $aboutContent = apply_filters('the_content', $about->post_content);
 
         <div class="col-md-4 flip-images">
 
-                <div class="f1_container">
-                    <div class="f1_card shadow">
-                        <div class="front face">
-                            <img src="<?php the_field('about_image1', 7); ?>"/>
-                        </div>
-                        <div class="back face center">
-                            <a href="<?php bloginfo('template_directory'); ?>/pdfs/CV.pdf">Read My CV</a>
-                        </div>
-                    </div>
-                </div>
+            <a href="#" data-toggle="modal" data-target="#about_CV"><img src="<?php the_field('about_image1', 7); ?>"/></a>
 
             <img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="alt text" class="about-logo"/>
 
+        </div>
+
+        <div class="portfolio-modal modal fade" id="about_CV" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl">
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="modal-body">
+                                <!-- Project Details Go Here -->
+                                <div class="col-md-12" style="padding-bottom: 15px;">
+                                    <img src="<?php bloginfo('template_directory'); ?>/img/cv.jpg">
+                                </div>
+
+                                <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div class="col-md-8 about-content">
